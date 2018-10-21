46fedc5aa207e0f86093426d3cedecf58347c109
Buddy Smith <buddyjasmith@gmail.com>
2018-10-20 22:40:58 -0500

    Update and rename node.cs to Node.cs

--- CSI_Prog5/Node.cs
+++ CSI_Prog5/Node.cs
@@ -1,4 +1,10 @@
-﻿namespace CSIGame
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI_Prog5
 {
     class Node
     {
