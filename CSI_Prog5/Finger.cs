ea5705112b45079814fae2ea93324c2b009f79a3
Buddy Smith <buddyjasmith@gmail.com>
2018-10-20 22:35:39 -0500

    Update Finger.cs

--- CSI_Prog5/Finger.cs
+++ CSI_Prog5/Finger.cs
@@ -1,25 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSIGame
+namespace testingGrounds
 {
+    //Class : Finger (inherits from Analyzer)
+    //Class Members: All inherited from Analyzer Class
+    //               const string V
+    //Methods:override Randomize()
+    //        override CheckIfClue()
+    //        override ToString()
+    //        Inherited constructors from base class
     class Finger : Analyzer
     {
         public Finger(short rows, short columns) : base(rows, columns)
-        {
+        { 
             Rand = new Random(Guid.NewGuid().GetHashCode());
-
-            //Generate up to 3 FingerPrint Clues per board
-            NumClues = Rand.Next(1, 3);
             for (int i = 0; i < NumClues; i++)
-                Randomize();
+                Randomize(i);
         }
 
-
-        public override void Randomize()
+        //override Randomizer()
+        //Parameters:none
+        //Return:void
+        //Description: Creates random instances of clues on the 
+        //             matrix, sets clue type to Fingerprint
+        public override void Randomize(int i)
         {
             int x,
                 y;
@@ -29,21 +37,42 @@ namespace CSIGame
             Rand = new Random(Guid.NewGuid().GetHashCode());
             y = Rand.Next(1, Columns);
             Array[x, y].ClueType = V;
+            Array[x, y].IsClue = true;
+            base.RCoord[i] = x;
+            base.CCoord[i] = y;
 
 
         }
+        //override CheckIfClue
+        //Parameters: short c and short r
+        //Return: bool
+        //Description: Checks if a clue resides in the matrix 
+        //             at Array[r,c]. The element is mark as 
+        //             checked always.Returns true or false
+        //             if element is a clue
         public override bool CheckIfClue(short c, short r)
         {
+            NumGuesses++;
             XPos = r;
             YPos = c;
             Array[r, c].BeenChecked = true;
             if (Array[r, c].ClueType == V)
             {
+                RCoord.RemoveAt(0);
+                CCoord.RemoveAt(0);
                 Array[r, c].BeenFound = true;
+                Array[r, c].Character = '@';
+                NumGuesses++;
                 return true;
             }
+           
             return false;
         }
+
+        //override ToString()
+        //Parameters: none
+        //Return: string
+        //Description: Returns a string contiaining 
         public override string ToString()
         {
             string ret = "";
@@ -52,11 +81,14 @@ namespace CSIGame
                 for (int c = 0; c < Columns; c++)
                 {
                     ret += Array[r, c].Character.ToString();
+                    ret += " ";
                 }
 
             }
             return ret;
         }
-        private const string V = "FingerPrint";
+     
+
+       
     }
 }
